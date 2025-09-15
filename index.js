63d70c1e45022994417b3abcfa0e5d2f3499a4e0
robiulalam7 <robiulbubt7@gmail.com>
2025-09-16 02:20:32 +0600

    added all funtionality

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
-const copyButtons = document.querySelectorAll(".copy-click");
-const copyCount = document.getElementById("copyCount");
-let count = parseInt(copyCount.textContent);
 
-copyButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    count++;
-    copyCount.textContent = count;
-    //alert("Number of copies: " + count);
-  });
+const copyCountEl = document.getElementById("copyCount");
+const heartCountEl = document.getElementById("heartCount");
+const callHistory = document.getElementById("callHistory");
+const clearHistory = document.getElementById("clearHistory");
+const callCoinEl = document.getElementById("callCoin"); 
+
+let copyCount = parseInt(copyCountEl?.textContent) || 0;
+let heartCount = parseInt(heartCountEl?.textContent) || 0;
+let callCoin = parseInt(callCoinEl?.textContent) || 100; 
+
+document.body.addEventListener("click", (e) => {
+  const target = e.target;
+
+ // copy
+  if (target.closest(".copy-click")) {
+    copyCountEl.textContent = ++copyCount;
+
+    const card = target.closest(".card-child");
+    const contactNumber = card.querySelector(".contact-number").textContent.trim();
+
+    navigator.clipboard.writeText(contactNumber).then(() => {
+      alert("Number Copied")
+    }).catch(err => console.error("Failed to copy:", err));
+  }
+
+  if (target.closest(".heart-count")) {
+    heartCountEl.textContent = ++heartCount;
+  }
+
+  if (target.closest(".call-click")) {
+    if (callCoin < 20) {
+      alert("Not enough coin");
+      return; 
+    }
+
+    // cut coin
+    callCoin -= 20;
+    callCoinEl.textContent = callCoin;
+
+    const card = target.closest(".card-child");
+    const title = card.querySelector("h2").textContent.trim();
+    const number = card.querySelector(".contact-number").textContent.trim();
+
+    const now = new Date().toLocaleTimeString();
+
+    const entry = document.createElement("div");
+    entry.className = "rounded-lg p-2 mb-2 shadow-sm bg-gray-200";
+    entry.innerHTML = `
+      <div class="flex justify-between items-center p-1">
+        <div>
+          <div class="font-bold">${title}</div>
+          <div class="text-gray-400">${number}</div>
+        </div>
+        <div class="text-xs text-black">${now}</div>
+      </div>
+    `;
+
+    callHistory.prepend(entry);
+  }
 });
-// heart count;
-const heartButtons = document.querySelectorAll(".heart-count");
-const heartCount = document.getElementById("heartCount");
-let hcount = parseInt(heartCount.textContent);
 
-heartButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    hcount++;
-    heartCount.textContent = hcount;
-  });
+
+clearHistory?.addEventListener("click", () => {
+  callHistory.innerHTML = "";
 });
